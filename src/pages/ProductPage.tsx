bc3ce704835ff3c86e05cf71696cb51c1c5e8bce
Nick Mulder <nmulder@outlook.com>
2023-05-18 18:35:25 -0500

    init commit

---
+++ src/pages/ProductPage.tsx
@@ -0,0 +1,11 @@
+import { products } from "../data/products";
+
+export function ProductPage() {
+  return (
+    <div className="text-center p-5">
+      <h2 className="text-xl font-bold text-slate-600">
+        Here are some great tools for React
+      </h2>
+    </div>
+  );
+}
